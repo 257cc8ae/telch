eeb499af9074882b204cff94ef5e6394b84832a8
anyfre <kny70dr@gmail.com>
2020-02-29 08:20:34 +0900

    Gitにファイル登録

---
+++ js/script.js
@@ -0,0 +1,35 @@
+(() => {
+
+    const rippleEffect = (event) => {
+
+        let target = event.target;
+
+        if (!target) {
+            return;
+        }
+
+        const cover = document.createElement('span');
+        const coverSize = target.offsetWidth;
+        const loc = target.getBoundingClientRect();
+
+        const x = event.pageX - loc.left - window.pageXOffset - (coverSize / 2);
+        const y = event.pageY - loc.top - window.pageYOffset - (coverSize / 2);
+
+        const pos = `top: ${y}px; left: ${x}px; height: ${coverSize}px; width: ${coverSize}px;`;
+
+        target.appendChild(cover);
+        cover.setAttribute('style', pos);
+        cover.setAttribute('class', 'ripple-active');
+
+        setTimeout(() => {
+            cover.remove();
+        }, 2000);
+    };
+
+    document.addEventListener('DOMContentLoaded', () => {
+        Array.from(document.querySelectorAll('.ripple')).forEach((elem) => {
+            elem.addEventListener('click', rippleEffect)
+        });
+    });
+
+})();
